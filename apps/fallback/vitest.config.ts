a9437042ad688e77416a08b3fea7db6b3a63a8df
Nico <nicobdx@icloud.com>
2026-04-24 15:42:55 +0200

    fix builds

---
+++ apps/fallback/vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig, mergeConfig } from "vitest/config";
+
+import viteConfig from "./vite.config";
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      include: ["./test/**/*.{test,spec}.?(c|m)[jt]s?(x)"],
+      environment: "jsdom",
+      setupFiles: ["./test/setup.ts"],
+      globalSetup: ["./test/global-setup.ts"],
+      testTimeout: 60_000,
+    },
+  }),
+);
